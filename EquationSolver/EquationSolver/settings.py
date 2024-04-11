51277a97678a888fa33050ec161f9a459718bbc9
Vaibhav Goyal <89004991+vabugoyal@users.noreply.github.com>
2024-04-11 14:17:14 +0530

    deployment commit 1

--- EquationSolver/EquationSolver/settings.py
+++ EquationSolver/EquationSolver/settings.py
@@ -130,3 +130,9 @@ STATICFILES_DIRS = [
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles_build", 'static')
 
+if not DEBUG:
+    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
+    # and renames the files with unique names for each version to support long-term caching
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
